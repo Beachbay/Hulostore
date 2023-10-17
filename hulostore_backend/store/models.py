d70e10dd15777917f2fc957bfcb5674e10e7c889
Beachbay <strandvikfilip@gmail.com>
2023-10-17 11:30:36 +0200

    created store-app and made models

---
+++ hulostore_backend/store/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
