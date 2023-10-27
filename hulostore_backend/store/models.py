2db28f8cddfe82cce29ff73e1f99a209fde68de6
Beachbay <strandvikfilip@gmail.com>
2023-10-27 12:20:51 +0200

    Models change

--- hulostore_backend/store/models.py
+++ hulostore_backend/store/models.py
@@ -8,10 +8,17 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(null=True, blank=True, default="")
 
-class Cart(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+class Order(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    adress = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zipCode = models.CharField(max_length=255)
 
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
